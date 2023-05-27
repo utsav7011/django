b5056554db0e80d820d2e921d5aab64190d2311b
utsav7011 <utsav7011@gmail.com>
2023-05-28 00:50:17 +0530

    for loop conditional statements and using include exclude in the django module

--- djangoProject/pr1/pr1/views.py
+++ djangoProject/pr1/pr1/views.py
@@ -4,7 +4,13 @@ from django.shortcuts import render
 def homePage(request):
     data = {
         'title':'Home Page -_-',
-        "h1": "this is the heading 1 from the django to HTML"
+        "h1": "this is the heading 1 from the django to HTML",
+        'cList': ['php', 'java','c++', 'Django'],
+        'numbers':[10, 20, 40, 30, 60, 50],
+        'studentDetails': [
+            {'name':'one', 'phone':9999999999},
+            {'name':'two', 'phone':8888888888}
+        ]
     }
     return render(request, "index.html", data)
 
